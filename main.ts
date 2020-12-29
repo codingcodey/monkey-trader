6c386693a9540dcab7a0920e70f1fe93cbd607ef
codingcodey <76607086+codingcodey@users.noreply.github.com>
2020-12-29 15:32:42 +1100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+let coin = 12
+let apple = 2
+let applePrice = 3
+let strawberry = 3
+let strawberryPrice = 6
+let peach = 7
+let peachPrice = 3
+scene.setBackgroundColor(5)
+game.splash("You have " + coin + " coins")
+coin = coin + apple * applePrice
+game.splash("After sold apples, you have " + coin + " coins")
+coin = coin + strawberry * strawberryPrice
+game.splash("After sold strawberries, you have " + coin + " coins")
+coin = coin + peach * peachPrice
+game.splash("After sold peaches, you have " + coin + " coins")
