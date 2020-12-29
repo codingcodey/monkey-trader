840e3f3c807d8e67c80c3291fc7aea6579e2bd7b
codingcodey <76607086+codingcodey@users.noreply.github.com>
2020-12-29 15:40:34 +1100

    Add messages

--- main.ts
+++ main.ts
@@ -1,3 +1,5 @@
+scene.setBackgroundColor(5)
+game.splash("Hi!")
 let coin = 12
 let apple = 2
 let applePrice = 3
@@ -5,7 +7,6 @@ let strawberry = 3
 let strawberryPrice = 6
 let peach = 7
 let peachPrice = 3
-scene.setBackgroundColor(5)
 game.splash("You have " + coin + " coins")
 coin = coin + apple * applePrice
 game.splash("After sold apples, you have " + coin + " coins")
@@ -13,3 +14,4 @@ coin = coin + strawberry * strawberryPrice
 game.splash("After sold strawberries, you have " + coin + " coins")
 coin = coin + peach * peachPrice
 game.splash("After sold peaches, you have " + coin + " coins")
+game.splash("Thanks for coming!")
